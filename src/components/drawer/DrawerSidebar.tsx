cba835566e9c1579b9771a0bbeff5dc7838517fa
Aahmed-Hossain <aahmdctec@gmail.com>
2024-04-25 11:25:52 +0600

    drawer set

--- src/components/drawer/DrawerSidebar.tsx
+++ src/components/drawer/DrawerSidebar.tsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React from 'react';
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import { TbTransactionBitcoin } from "react-icons/tb";
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 const dashboardPaths = [
     {
       title: "Dashboard",
@@ -6,35 +13,45 @@ const dashboardPaths = [
       icon: DashboardIcon,
     },
     {
-      title: "Specialties",
-      path: `/specialties`,
-      icon: FolderSpecialIcon,
+      title: "Transactions",
+      path: `transactions`,
+      icon: TbTransactionBitcoin,
     },
     {
-      title: "Doctors",
-      path: `/doctors`,
-      icon: MedicalInformationIcon,
-    },
-    {
-      title: "Schedules",
-      path: `/schedules`,
+      title: "Calendar",
+      path: `calendar`,
       icon: CalendarMonthIcon,
-    },
-    {
-      title: "Appointments",
-      path: `/appointments`,
-      icon: DateRangeIcon,
-    },
-    {
-      title: "Reviews",
-      path: `/reviews`,
-      icon: RateReviewIcon,
     },
   ];
 const DrawerSidebar = () => {
+    const pathName=usePathname()
     
   return (
-    <div>DrawerSidebar</div>
+    <Box>
+        <List>
+            {
+              dashboardPaths.map((item, index)=>
+            <Link href={`/dashboard/${item.path}`} key={index}>
+                <ListItem key={index}
+             disablePadding
+             sx={{
+                  ...(pathName === `/dashboard/${item.path}`
+                    ? { borderRight: "3px solid blue", "& svg": { color: "blue" },my:2 }
+                    : {}),
+                }}
+             >
+              <ListItemButton>
+                <ListItemIcon>
+                  {item.icon&&<item.icon/>}
+                </ListItemIcon>
+                <ListItemText primary={item.title} />
+              </ListItemButton>
+            </ListItem>
+              
+              </Link>) 
+            }
+        </List>
+    </Box>
   )
 }
 
