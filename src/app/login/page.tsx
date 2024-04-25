41553900778ead5cabdab62b41ccb36d1082ee09
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-04-25 19:37:04 +0600

    login page initiated

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <main className="">
+      <h2 className="text-center font-extrabold text-6xl text-yellow-400 mt-6">
+        Login Page
+      </h2>
+    </main>
+  );
+}
