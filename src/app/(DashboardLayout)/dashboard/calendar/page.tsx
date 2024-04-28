3235e218471d09e4d5f7d4388057216b6a11f1af
Aahmed-Hossain <aahmdctec@gmail.com>
2024-04-28 21:55:11 +0600

    updated the calendar section

--- src/app/(DashboardLayout)/dashboard/calendar/page.tsx
+++ src/app/(DashboardLayout)/dashboard/calendar/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CalendarPage = () => {
   return (
-    <div>CalendarPage</div>
+    <div>CalendarPage for showing daily income and expense update CalendarPage for showing daily income and expense update</div>
   )
 }
 
